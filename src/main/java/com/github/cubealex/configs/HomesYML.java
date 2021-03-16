43eee485782236273b20ea6912d80882d758dd9b
CubeAlex <cubealex2019@gmail.com>
2021-03-16 05:36:37 +0100

    Added Home and SetHome Command #2

---
+++ src/main/java/com/github/cubealex/configs/HomesYML.java
@@ -0,0 +1,46 @@
+package com.github.cubealex.configs;
+
+import com.github.cubealex.main.Main;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class HomesYML {
+
+    private static File configFile = new File(Main.getPlugin().getDataFolder(), "homes.yml");
+
+    private static FileConfiguration config = new YamlConfiguration();
+
+    public static void createConfig () {
+
+        //Checks if ConfigFile doesnt exist
+        if (!configFile.exists()) {
+
+            configFile.getParentFile().mkdirs();
+            Main.getPlugin().saveResource("homes.yml", false);
+
+        }
+
+        try {
+
+            config.load(configFile);
+
+        }catch (IOException | InvalidConfigurationException e ) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static FileConfiguration getConfig() {
+        return config;
+    }
+
+    public static File getConfigFile() {
+        return configFile;
+    }
+}
