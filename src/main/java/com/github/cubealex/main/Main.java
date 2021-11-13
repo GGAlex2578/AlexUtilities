1ad10245bb1440dafec812daa7ba0ead561f9134
GGAlex <alexander.katzki@outlook.com>
2021-11-13 14:48:57 +0100

    Deleted Backpack Command

--- src/main/java/com/github/cubealex/main/Main.java
+++ src/main/java/com/github/cubealex/main/Main.java
@@ -1,9 +1,7 @@
 package com.github.cubealex.main;
 
-import com.github.cubealex.commands.BackpackCommand;
 import com.github.cubealex.commands.CoordsCommand;
 import com.github.cubealex.commands.HomeCommand;
-import com.github.cubealex.configs.BackpackYML;
 import com.github.cubealex.configs.CoordsYML;
 import com.github.cubealex.configs.HomesYML;
 import com.github.cubealex.listener.JoinListener;
@@ -17,60 +15,38 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
         plugin = this;
-
         this.getLogger().info(Utils.prefix + "§aDas Plugin wird gestartet!");
-
         try {
-
             this.getLogger().info(Utils.prefix + "§aDas Plugin wird geladen...");
-
             //Try register all Commands and Listener
             register();
-
             //Initialize Config
             this.saveDefaultConfig();
-
             //Load all Configs
             HomesYML.createConfig();
             CoordsYML.createConfig();
-            BackpackYML.createConfig();
-
         }catch (Exception e) {
-
             e.printStackTrace();
-
         }
-
         this.getLogger().info(Utils.prefix + "§aDas Plugin wurde geladen!");
-
     }
 
     @Override
     public void onDisable() {
-
         this.getLogger().info(Utils.prefix + "§4Das Plugin wurde gestoppt!");
-
     }
 
     private void register() {
-
         //Registers Commands
         this.getCommand("home").setExecutor(new HomeCommand());
         this.getCommand("sethome").setExecutor(new HomeCommand());
         this.getCommand("coords").setExecutor(new CoordsCommand());
-        this.getCommand("backpack").setExecutor(new BackpackCommand());
-
         //Registers CommandTabCompleter
         this.getCommand("coords").setTabCompleter(new CoordsTabCompleter());
-
         //Registers Listeners
         PluginManager pm = this.getServer().getPluginManager();
-
         pm.registerEvents(new JoinListener(), this);
-        pm.registerEvents(new BackpackCommand(), this);
-
     }
 
     public static Main getPlugin() {
